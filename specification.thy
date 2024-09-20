7417d3c3862790ed828a40cf5551a49cbdd7bd4d
Santiago Firpo <s.firpo@edu.pucrs.br>
2024-09-20 19:02:28 -0300

    Labeled primrec definitions

--- specification.thy
+++ specification.thy
@@ -7,28 +7,28 @@ datatype 'a tree = Empty | Node "'a tree" 'a "'a tree"
 
 (* Definição da função cat *)
 primrec cat :: "'a list ⇒ 'a list ⇒ 'a list" where
-  "cat [] l = l" |
-  "cat (h # t) l = h # (cat t l)"
+  cat_base: "cat [] l = l" |
+  cat_induction: "cat (h # t) l = h # (cat t l)"
 
 (* Definição da função size (tamanho) *)
 primrec size :: "'a list ⇒ nat" where
-  "size [] = 0" |
-  "size (h # t) = 1 + size t"
+  size_base: "size [] = 0" |
+  size_induction: "size (h # t) = 1 + size t"
 
 (* Definição da função numnodes (numnodos) *)
 primrec numnodes :: "'a tree ⇒ nat" where
-  "numnodes Empty = 0" |
-  "numnodes (Node L x R) = 1 + numnodes L + numnodes R"
-
-(* Definição da função content (conteudo) *)
-primrec content :: "'a tree ⇒ 'a list" where
-  "content Empty = []" |
-  "content (Node L x R) = x # (cat (content L) (content R))"
+  numnodes_base: "numnodes Empty = 0" |
+  numnodes_induction: "numnodes (Node L x R) = 1 + numnodes L + numnodes R"
 
 (* Definição da função mirror (espelho) *)
 primrec mirror :: "'a tree ⇒ 'a tree" where
-  "mirror Empty = Empty" |
-  "mirror (Node L x R) = Node (mirror R) x (mirror L)"
+  mirror_base: "mirror Empty = Empty" |
+  mirror_induction: "mirror (Node L x R) = Node (mirror R) x (mirror L)"
+
+(* Definição da função content (conteudo) *)
+primrec content :: "'a tree ⇒ 'a list" where
+  content_base: "content Empty = []" |
+  content_induction: "content (Node L x R) = x # (cat (content L) (content R))"
 
 (* lemma da prova 1 para ser reusado na prova 2 *)
 lemma cat_size: "size (cat L1 L2) = size L1 + size L2"
@@ -58,8 +58,8 @@ proof (induct A)
   case Empty
   show ?case
   proof -
-    have "numnodes Empty = 0" by simp
-    have "size (content Empty) = 0" by simp
+    have "numnodes Empty = 0" by (simp only: numnodes_base)
+    have "size (content Empty) = 0" by (simp only: size_base content_base)
     thus ?thesis by simp
   qed
 next
@@ -69,8 +69,8 @@ next
   
   show ?case
   proof -
-    have "numnodes (Node L x R) = 1 + numnodes L + numnodes R" by simp
-    have "content (Node L x R) = x # cat (content L) (content R)" by simp
+    have "numnodes (Node L x R) = 1 + numnodes L + numnodes R" by (simp only: numnodes_induction)
+    have "content (Node L x R) = x # cat (content L) (content R)" by (simp only: content_induction)
     have "size (content (Node L x R)) = 1 + size (cat (content L) (content R))" by simp
     have "size (cat (content L) (content R)) = size (content L) + size (content R)" by (simp only: cat_size)
     have numnodos_tamanho: "numnodes (Node L x R) = 1 + size (content L) + size (content R)"
